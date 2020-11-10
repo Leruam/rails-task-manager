3e7d0624f7a8163ffbf20c21215f719ce1e8fd11
antoine maurel <antoine.maurel@gmail.com>
2020-11-10 14:37:34 +0100

    done the add new task (new and create)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,20 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new # on en a besoin pour instancier le form
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
